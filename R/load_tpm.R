7e4ffa04fd250a366663520d78708ca46b32095b
Jordy Bollon <jbollon@iit.local@IITCMP3VDAWS010.iit.local>
2023-06-16 12:17:25 +0200

    Add warning message and update the vignettes.

--- R/load_tpm.R
+++ R/load_tpm.R
@@ -2,8 +2,18 @@ load_tpm = function(protein_name, mRNAfile){
 	tpm = fread(file = mRNAfile, sep = '\t', header = TRUE)
 	matches = match(protein_name, tpm$isoname)
 	tpm = tpm$tpm[matches]
-	tpm[is.na(tpm)] = 0
-
+	
+	if(sum(is.na(tpm)) > 0){
+	  not_matched = which(is.na(tpm))
+	  print("!!! WARNING !!!")
+	  print(glue("Found {length(matches) - length(not_matched)} correspondances between transcripts and protein isoforms.
+	              The total number of protein isoforms is {length(protein_name)}. For {length(not_matched)} trascript(s) we set the tpm equal to 0.")
+	        )
+	  print("If the number of correspondances is relatively high,
+	        check if the format name of the protein isoforms is consistent with those of the transcripts.")
+	  tpm[is.na(tpm)] = 0
+	}
+	
 	# normalize TPMs:
 	tpm = tpm/sum(tpm) * 10^6
 	
